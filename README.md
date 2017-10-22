9a4df98d24f4e4b60038acdcafc903b38d837eb5
Carmelo <carmelo@ns3309489.ip-5-135-160.eu>
2017-10-22 21:45:10 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# bpbot
+Telegram bot to store Blood pressure data. It provides historical data and graphs to the end user.
---
+++ telegramBPserver.js
@@ -0,0 +1,174 @@
+const MongoClient = require('mongodb').MongoClient
+const Telegraf = require('telegraf')
+const env = require('./env.json')
+
+
+var plotly = require('plotly')(env.plotly.username, env.plotly.token)
+var fs = require('fs');
+
+const app = new Telegraf(env.TOKEN)
+
+app.hears(/^hi$/i, (ctx) => ctx.reply('Hey there!'))
+
+function storeBP(_id, _datetime, _sys, _dist) {
+        MongoClient.connect('mongodb://localhost/bp', function (err, db) {
+
+                if (err) throw err
+
+                db.collection("data").insertOne({
+			id: _id,
+			datetime: _datetime,
+			sys: _sys,
+			dist: _dist
+		})
+        })
+}
+
+
+function validBP(sys, dist) {
+  
+  if (sys < 50 || sys > 300 || dist < 20 || dist > 150) {
+    return false
+  }
+
+  return true
+}
+
+
+const getBP = /^bp [0-9.]+ [0-9.]+/i
+app.hears(getBP, ctx => {
+
+  console.log(ctx.message)
+
+  const tokens = ctx.message.text.split(/\s+/)
+
+  var sys = parseFloat(tokens[1])
+  var dist = parseFloat(tokens[2])
+  var datetime = new Date(parseInt(ctx.message.date) * 1000)
+  var id = ctx.message.from.id
+
+
+  if (sys < dist) {
+    var temp = sys
+    sys = dist
+    dist = temp
+  }
+
+  if (validBP(sys, dist)) {
+    ctx.reply('Thank you for providing your BP readings')
+    ctx.reply('You have entered ' + sys + '/' + dist)
+    storeBP(id, datetime, sys, dist)
+  } else {
+    ctx.reply('It seems like you have entered an invalid BP reading. Try again.')
+  }
+
+})
+
+
+function getBPs(id, success, failure) {
+
+   MongoClient.connect('mongodb://localhost/bp', function (err, db) {
+      if (err) {
+         failure()
+         return
+      }
+
+      db.collection("data").find({id: id}).toArray((err, res) => {
+
+      if (err) {
+         failure()
+         return
+      }
+
+      success(res)
+
+     db.close()
+     })
+   })
+
+}
+
+
+app.command('/history', (ctx) => {
+
+   getBPs(ctx.message.from.id,
+   (res)=>{
+      const strings = res.map((e) => e.datetime.toLocaleString('en-GB') + " " + e.sys + "/" + e.dist)
+
+      if (typeof(strings) == 'Object') {
+         ctx.reply(strings)
+      } else {
+         console.log(strings)
+         const stringList = strings.join("\n")
+         ctx.reply(stringList)
+      }
+   },
+   () => {
+      console.log("Error occurred")
+   })
+})
+
+
+app.command('/graph', (ctx) => {
+
+   getBPs(ctx.message.from.id,
+   (res)=>{
+      const x = res.map((e) => e.datetime)
+      const sys = res.map((e) => e.sys)
+      const dist = res.map((e) => e.dist)
+
+
+      var trace1 = {
+          x: x,
+          y: sys,
+          type: "scatter",
+          name: "Systolic"
+      }
+
+      var trace2 = {
+          x: x,
+          y: dist,
+          type: "scatter",
+          name: "Diastolic"
+      }
+
+      var layout = {
+         title: "Blood pressure values",
+         xaxis: {title: "Date"},
+         yaxis: {title: "BP [mmHg]"}
+      }
+
+      var figure = { data: [trace1, trace2], layout: layout }
+
+      var imgOpts = {
+          format: 'png',
+          width: 1000,
+          height: 500
+      }
+
+     plotly.getImage(figure, imgOpts, function (error, imageStream) {
+        if (error) return console.log (error)
+        const nameImg = ctx.message.from.id + "-" + Date.now() + ".png"
+
+        ctx.replyWithPhoto({source: imageStream})
+
+     })
+
+   },
+   () => {
+      console.log("Error occurred")
+   })
+
+
+})
+
+
+app.command('start', (ctx) => {
+   ctx.reply('Hello. Thank you for joining. ☺️',
+   Telegraf.Extra.markup((markup) => {return markup.keyboard(['/graph', '/history'])}))
+})
+
+app.on('sticker', (ctx) => ctx.reply('👍'))
+
+app.startPolling()
+
